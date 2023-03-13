b951261039e22b2e0875369d959f850f6ae99665
Sara Fotros <55629228+sarafotros@users.noreply.github.com>
2023-03-13 09:43:50 +0000

    alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react';
-import ListGroup from './components/ListGroup';
-import Message from './Message';
+import { useState } from "react";
+import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
+import Message from "./Message";
 
-
-function App (){
+function App() {
   const listItem = ["London", "paris", "New York", "Sydney"];
   const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
-  return <div><ListGroup items={listItem} heading="Cities" onSelectItem={handleSelectItem}/></div>
+    console.log(item);
+  };
+  return (
+    <div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+      <ListGroup
+        items={listItem}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
